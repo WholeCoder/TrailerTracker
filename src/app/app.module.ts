e4651ccd1efde9e47ab3bd891dee071d6657e1bf
WholeCoder <m2gt@at>
2017-02-23 23:03:18 -0500

    Added separate outlets for different components.  Not working though.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,42 @@ import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ng2-bootstrap';
 import { TabsModule } from 'ng2-bootstrap';
 import { MyNewCoonentComponent } from './my-new-coonent/my-new-coonent.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 //git push origin [name_of_your_new_branch]
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewCoonentComponent
+    MyNewCoonentComponent,
+    NavBarComponent
   ],
   imports: [
-    TabsModule.forRoot(),
-    AlertModule.forRoot(),
+    TabsModule,
+    AlertModule,
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(
+      [
+            {
+                path: '',
+                component: NavBarComponent,
+                outlet: 'nav-bar'
+            },
+            {
+                path: '',
+                component: MyNewCoonentComponent,
+                outlet: 'main-content'
+            }
+        ]
+
+
+    )
     // MyNewCoonentComponent
   ],
   providers: [],
