cd28cb9deac5de720a4df96af8121924889ab75a
Ruben <rpierich@hotmail.com>
2017-03-12 19:08:57 -0400

    Learned how to do bulk inserts.

--- load_dummy_trailers_into_postgres.js
+++ load_dummy_trailers_into_postgres.js
@@ -1,44 +1,68 @@
+var config = require('./config').config;
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://postgres:pgsGood&Plenty@localhost:5432/postgres');
+var sequelize = new Sequelize(config.database_url);
+console.log("database_url == " + config.database_url);
+
+var Trailer = require('./trailer-model').Trailer;
+
+var trlrs =  [{"_id":"56382f87ff7831c0487f65ed",
+        "unitnumber":"174929",
+        "customer":"CR ENGLAND",
+        "account":"OTR",
+        "vehicletype":"TRACTOR:  CONDO",
+        "location":"FRS - (GRANTVILLE PA)",
+        "datersnotified":"","dateapproved":"",
+        "estimatedtimeofcompletion":"",
+        "status1":"100% COMPLETE:  RESERVED",
+        "status2":"","status3":"",
+        "note":"Was towed in from virginia *****wreck*****",
+        "__v":0,"whentobearchived":null,
+        "dateauthorized":"2016-03-27T21:25:29.044Z",
+        "authorizedinitials":"rmp174929"
+        },{"_id":"56382f87ff7831c0487f65ef","unitnumber":"21335","customer":"CR ENGLAND","account":"OTR","vehicletype":"TRAILER:  REEFER VAN","location":"FRS - (GRANTVILLE PA)","datersnotified":"10/01/2015","dateapproved":"10/07/2015","estimatedtimeofcompletion":"10/12/2015","status1":"100% COMPLETE:  READY FOR P/U","status2":"","status3":"","note":"AP# 397905","__v":0,"whentobearchived":null},
+            {"_id":"56382f87ff7831c0487f65f0","unitnumber":"53090","customer":"CHAMBERSBURG WASTE PAPER","account":"FRS ACCOUNT","vehicletype":"TRAILER:  DRY VAN","location":"FRS - (GRANTVILLE PA)","datersnotified":"10/06/2015","dateapproved":"10/20/2015","estimatedtimeofcompletion":"","status1":"100% COMPLETE:  READY FOR P/U","status2":"","status3":"","note":"","__v":0,"whentobearchived":null}];
+
+            for (var i = 0; i < trlrs.length; i++)
+            {
+              console.log("trlrs.unitnumber == "+trlrs[i].unitnumber);
+            }
+            function getProps(obj)
+            {
+              var p = "";
+              for (var prop in obj)
+              {
+                p += prop + ' == ' + obj[prop] + '\n';
+              }
+              return p;
+            }
 
 
-var Trailer = sequelize.define('Trailer', {
-
-     "unitnumber": Sequelize.STRING,
-     "customer": Sequelize.STRING,
-     "account": Sequelize.STRING,
-     "vehicletype": Sequelize.STRING,
-     "location": Sequelize.STRING,
-     "datersnotified": Sequelize.STRING,
-     "estimatedtimeofcompletion": Sequelize.STRING,
-     "status1": Sequelize.STRING,
-     "status2": Sequelize.STRING,
-     "note": Sequelize.STRING,
-     "dateauthorized": Sequelize.STRING,
-     "authorizedinitials": Sequelize.STRING
-
-});
-
-sequelize.sync().then(function() {
-  return Trailer.create({
-
-            "unitnumber":"174929",
-            "customer":"CR ENGLAND",
-            "account":"OTR",
-            "vehicletype":"TRACTOR:  CONDO",
-            "location":"FRS - (GRANTVILLE PA)",
-            "datersnotified":"","dateapproved":"",
-            "estimatedtimeofcompletion":"",
-            "status1":"100% COMPLETE:  RESERVED",
-            "status2":"","status3":"",
-            "note":"Was towed in from virginia *****wreck*****",
-            "whentobearchived":null,
-            "dateauthorized":"2016-03-27T21:25:29.044Z",
-            "authorizedinitials":"rmp174929"
-
-  });
-}).then(function(jane) {
-  console.log(jane.get({
-    plain: true
-  }));
-});
+for (var i = 0; i < trlrs.length; i++)
+  {
+    Trailer.upsert(trlrs[i]);
+  //   var thatIndex = i;
+  // sequelize.sync().then(function() {
+  //   console.log("obj == " + trlrs[thatIndex].unitnumber);
+  //   var trailerObject = {
+  //             "unitnumber":"unitnumber",
+  //             "customer":"CR ENGLAND",
+  //             "account":"OTR",
+  //             "vehicletype":"TRACTOR:  CONDO",
+  //             "location":"FRS - (GRANTVILLE PA)",
+  //             "datersnotified":"","dateapproved":"",
+  //             "estimatedtimeofcompletion":"",
+  //             "status1":"100% COMPLETE:  RESERVED",
+  //             "status2":"","status3":"",
+  //             "note":"Was towed in from virginia *****wreck*****",
+  //             "whentobearchived":null,
+  //             "dateauthorized":"2016-03-27T21:25:29.044Z",
+  //             "authorizedinitials":"rmp174929"
+  //
+  //   }
+  //   return Trailer.create(trailerObject);
+  // }).then(function(jane) {
+  //   console.log(jane.get({
+  //     plain: true
+  //   }));
+  // });
+}
--- trailer-model.js
+++ trailer-model.js
@@ -1,23 +1,21 @@
-var Sequelize = require('sequelize'),
-    bcrypt = require('bcryptjs'),
+var bcrypt = require('bcryptjs');
+    var config = require('./config').config;
+    var Sequelize = require('sequelize');
+    var sequelize = new Sequelize(config.database_url);
 
-    SALT_WORK_FACTOR = 10;
+  exports.Trailer = sequelize.define('Trailer', {
 
-exports.CreateTrailer = function(sequelize) {
-    var Trailer = sequelize.define('Trailer', {
-        "unitnumber": Sequelize.STRING,
-        "customer": Sequelize.STRING,
-        "account": Sequelize.STRING,
-        "vehicletype": Sequelize.STRING,
-        "location": Sequelize.STRING,
-        "datersnotified": Sequelize.STRING,
-        "estimatedtimeofcompletion": Sequelize.STRING,
-        "status1": Sequelize.STRING,
-        "status2": Sequelize.STRING,
-        "note": Sequelize.STRING,
-        "dateauthorized": Sequelize.STRING,
-        "authorizedinitials": Sequelize.STRING
-    })
+       "unitnumber": Sequelize.STRING,
+       "customer": Sequelize.STRING,
+       "account": Sequelize.STRING,
+       "vehicletype": Sequelize.STRING,
+       "location": Sequelize.STRING,
+       "datersnotified": Sequelize.STRING,
+       "estimatedtimeofcompletion": Sequelize.STRING,
+       "status1": Sequelize.STRING,
+       "status2": Sequelize.STRING,
+       "note": Sequelize.STRING,
+       "dateauthorized": Sequelize.STRING,
+       "authorizedinitials": Sequelize.STRING
 
-    return Trailer;
-}
+  });
