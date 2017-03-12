a0d85f344105da6a69e495dbb84517e56e2ab65d
Ruben <rpierich@hotmail.com>
2017-03-12 15:42:06 -0400

    Added user and sequalize code.

---
+++ load_dummy_trailers_into_postgres.js
@@ -0,0 +1,16 @@
+var DATABASE_URL = 'postgres://postgres:pgsGood&Plenty@localhost:5432/postgres';
+
+var Sequelize = require('sequelize'),
+    sequelize = new Sequelize(DATABASE_URL),
+    User = require('./user-model').CreateUser(sequelize);
+
+sequelize.sync().then(function() {
+  return User.create({
+    username: 'rpierich@hotmail.com',
+    password: 'asdf'
+  });
+}).then(function(jane) {
+  console.log(jane.get({
+    plain: true
+  }));
+});
