99868c645e41d949131f52fccbbd50330a302726
Ruben <rpierich@hotmail.com>
2017-03-13 10:23:41 -0400

    Getting closer. Build still broken though.

--- server/models/trailer-model.ts
+++ server/models/trailer-model.ts
@@ -1,4 +1,4 @@
-import * as Sequelize from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize';
 
 export interface TrailerAttributes {
   unitnumber:string;
@@ -15,41 +15,59 @@ export interface TrailerAttributes {
   authorizedinitials: string;
 }
 
-export interface TrailerInstance extends Sequelize.Instance<TrailerAttributes> {
-  // I'm exposing every DB column as an instance field to so that tsc won't complain.
-  id: number;
-  createdAt: Date;
-  updatedAt: Date;
-
-  unitnumber:string;
-  customer: string;
-  account:string;
-  vehicletype:string;
-  location: string;
-  datersnotified: string;
-  estimatedtimeofcompletion:string;
-  status1: string;
-  status2: string;
-  note: string;
-  dateauthorized: string;
-  authorizedinitials: string;
+export interface TrailerInstance extends Sequelize.Instance<TrailerInstance,TrailerAttributes>, TrailerAttributes {
 
 }
 
-export default function defineTrailer(sequelize: Sequelize.Sequelize) {
-  var Trailer = sequelize.define('Trailer', {
-    unitnumber:Sequelize.DataTypes.STRING,
-    customer:Sequelize.DataTypes.STRING,
-    account:Sequelize.DataTypes.STRING,
-    vehicletype:Sequelize.DataTypes.STRING,
-    location: Sequelize.DataTypes.STRING,
-    datersnotified: Sequelize.DataTypes.STRING,
-    estimatedtimeofcompletion:Sequelize.DataTypes.STRING,
-    status1: Sequelize.DataTypes.STRING,
-    status2:Sequelize.DataTypes.STRING,
-    note:Sequelize.DataTypes.STRING,
-    dateauthorized:Sequelize.DataTypes.STRING,
-    authorizedinitials:Sequelize.DataTypes.STRING
+// {
+//   // I'm exposing every DB column as an instance field to so that tsc won't complain.
+//   id: number;
+//   createdAt: Date;
+//   updatedAt: Date;
+//
+//   unitnumber:string;
+//   customer: string;
+//   account:string;
+//   vehicletype:string;
+//   location: string;
+//   datersnotified: string;
+//   estimatedtimeofcompletion:string;
+//   status1: string;
+//   status2: string;
+//   note: string;
+//   dateauthorized: string;
+//   authorizedinitials: string;
+//
+// }
+
+export default function defineTrailer(sequelize: Sequelize) {
+console.error("********************works****************");
+  function printProps(obj)
+  {
+    var str = "";
+    for (var prop in obj)
+    {
+      str += prop + '    ' + obj[prop] + '\n';
+    }
+    console.log("props == "+str);
+  }
+  console.log('*****************************');
+  printProps(DataTypes);
+  console.log('****************(new)');
+
+  var Trailer =sequelize.define('Trailer', {
+    unitnumber:DataTypes.STRING(),
+    customer:DataTypes.STRING(),
+    account:DataTypes.STRING(),
+    vehicletype:DataTypes.STRING(),
+    location: DataTypes.STRING(),
+    datersnotified: DataTypes.STRING(),
+    estimatedtimeofcompletion:DataTypes.STRING(),
+    status1: DataTypes.STRING(),
+    status2:DataTypes.STRING(),
+    note:DataTypes.STRING(),
+    dateauthorized:DataTypes.STRING(),
+    authorizedinitials:DataTypes.STRING()
 
   });
 
--- server/routes/trailers.ts
+++ server/routes/trailers.ts
@@ -1,13 +1,25 @@
 import { Router, Response, Request } from 'express';
 import * as Sequelize from 'sequelize';
 // let Trailer = require('../models/trailer-model');
-import * as defineTrailer from '../models/trailer-model';
+import { defineTrailer } from '../models/trailer-model';
 const trailerRouter: Router = Router();
 import { config } from '../config_db';
 
+function printProps(obj)
+{
+  var str = "";
+  for (var prop in obj)
+  {
+    str += prop + '    ' + obj[prop] + '\n';
+  }
+  console.log("props == "+str);
+}
 let sequelize:Sequelize.Sequelize = new Sequelize.Sequelize(new config().database_url);
-let Trailer = defineTrailer(sequelize);
-
+console.log("\n---------------\n");
+printProps(defineTrailer);
+console.log("\n---------------(end)\n");
+let Trailer = defineTrailer.defineTrailer(sequelize);
+//
 trailerRouter.get('/', (request: Request, response: Response) => {
   response.json(Trailer.findAll());
 });
