34c65bd263d08602776a21df4ce6f44e9d010064
Ruben <rpierich@hotmail.com>
2017-04-02 03:41:25 -0400

    Added option to form table forgot.

--- remove_hash_marks_from_percentages_in_status_column.sql
+++ remove_hash_marks_from_percentages_in_status_column.sql
@@ -14,29 +14,90 @@ group by t.status1
 
 
 
+-- Update percentages - status1
+-- if we clone a db then it may have extra # characters so run this
+-- script on that db to get rid of them.
 
-update "trailers" t
-  set status1 =  TEXTCAT('10%',SUBSTRING(T.status1 from 5))
-where status1 LIKE '10%'
+-- 100#% COMPLETE:  RELEASED TO CUSTOMER
 
-update "trailers" t
-  set status1 =  TEXTCAT('25%',SUBSTRING(T.status1 from 5))
-where status1 LIKE '25%'
+UPDATE "trailers" t
+SET status1 = TEXTCAT('100% ',SUBSTRING( status1 FROM 6))
+WHERE status1 LIKE '100#%'
 
-update "trailers" t
-  set status1 =  TEXTCAT('50%',SUBSTRING(T.status1 from 5))
-where status1 LIKE '50%'
+UPDATE "trailers" t
+SET status1 = TEXTCAT('25%  ',SUBSTRING( status1 FROM 7))
+WHERE status1 LIKE '25#%'
 
-update "trailers" t
-  set status1 =  TEXTCAT('75%',SUBSTRING(T.status1 from 5))
-where status1 LIKE '75%'
+UPDATE "trailers" t
+SET status1 = TEXTCAT('10%  ',SUBSTRING( status1 FROM 7))
+WHERE status1 LIKE '10#%'
 
-update "trailers" t
-  set status1 =  TEXTCAT('90%',SUBSTRING(T.status1 from 5))
-where status1 LIKE '90%'
+UPDATE "trailers" t
+SET status1 = TEXTCAT('50%  ',SUBSTRING( status1 FROM 7))
+WHERE status1 LIKE '50#%'
+
+UPDATE "trailers" t
+SET status1 = TEXTCAT('75%  ',SUBSTRING( status1 FROM 7))
+WHERE status1 LIKE '75#%'
+
+UPDATE "trailers" t
+SET status1 = TEXTCAT('90%  ',SUBSTRING( status1 FROM 7))
+WHERE status1 LIKE '90#%'
 
 
-update "trailers" t
-  set status1 =  TEXTCAT('100%',SUBSTRING(T.status1 from 6)) -- verifiy this 6 before executing!!!!!!!!!!!!!!!!!!!!!!!!!!
-where status1 LIKE '100%'
+
+
+
+
+-- status2
+UPDATE "trailers" t
+SET status2 = TEXTCAT('100% ',SUBSTRING( status2 FROM 6))
+WHERE status2 LIKE '100#%'
+
+UPDATE "trailers" t
+SET status2 = TEXTCAT('25%  ',SUBSTRING( status2 FROM 7))
+WHERE status2 LIKE '25#%'
+
+UPDATE "trailers" t
+SET status2 = TEXTCAT('10%  ',SUBSTRING( status2 FROM 7))
+WHERE status2 LIKE '10#%'
+
+UPDATE "trailers" t
+SET status2 = TEXTCAT('50%  ',SUBSTRING( status2 FROM 7))
+WHERE status2 LIKE '50#%'
+
+UPDATE "trailers" t
+SET status2 = TEXTCAT('75%  ',SUBSTRING( status2 FROM 7))
+WHERE status2 LIKE '75#%'
+
+UPDATE "trailers" t
+SET status2 = TEXTCAT('90%  ',SUBSTRING( status2 FROM 7))
+WHERE status2 LIKE '90#%'
+
+
+--status3
+
+UPDATE "trailers" t
+SET status3 = TEXTCAT('100% ',SUBSTRING( status3 FROM 6))
+WHERE status3 LIKE '100#%'
+
+UPDATE "trailers" t
+SET status3 = TEXTCAT('25%  ',SUBSTRING( status3 FROM 7))
+WHERE status3 LIKE '25#%'
+
+UPDATE "trailers" t
+SET status3 = TEXTCAT('10%  ',SUBSTRING( status3 FROM 7))
+WHERE status3 LIKE '10#%'
+
+UPDATE "trailers" t
+SET status3 = TEXTCAT('50%  ',SUBSTRING( status3 FROM 7))
+WHERE status3 LIKE '50#%'
+
+UPDATE "trailers" t
+SET status3 = TEXTCAT('75%  ',SUBSTRING( status3 FROM 7))
+WHERE status3 LIKE '75#%'
+
+UPDATE "trailers" t
+SET status3 = TEXTCAT('90%  ',SUBSTRING( status3 FROM 7))
+WHERE status3 LIKE '90#%'
 
