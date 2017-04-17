7a473c1afef18042d830de8c56b4584ac886bd8d
Ruben <rpierich@hotmail.com>
2017-04-16 23:49:33 -0400

    Got all test to pass.

--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -1,23 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import {TableDemoComponent} from './table.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {BrowserTestingModule} from '@angular/platform-browser/testing';
+import {TrailerService} from '../trailer.service';
+import {PassTrailerDataService} from '../pass-trailer-data.service';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {StatusService} from '../status.service';
 
-import { TableComponent } from './table.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
-describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+describe('TableDemoComponent', () => {
+  let component: TableDemoComponent;
+  let fixture: ComponentFixture<TableDemoComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableComponent ],
-      imports: [ BrowserTestingModule, RouterTestingModule, platformBrowserTesting() ]
+      declarations: [TableDemoComponent],
+      imports: [BrowserTestingModule, RouterTestingModule, HttpModule],
+      providers: [TrailerService, PassTrailerDataService, StatusService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TableComponent);
+    fixture = TestBed.createComponent(TableDemoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
